8fd3c14fdae28239c3e4b72e9660dd452aad7b78
Rai Velasquez Fermin <raivelasquezf@gmail.com>
2021-03-19 22:13:44 -0500

    Bootstrap

--- app.js
+++ app.js
@@ -9,21 +9,18 @@ const app = express();
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 
-console.log(datos)
-// app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(__dirname + "/public"));
+
 app.get('/', (req, res) => {
     res.render("index", { titulo: "cambios en back" })
-    // res.send("hola 2")
 })
 
 app.get('/servicios', (req, res) => {
     res.render("servicios", { servicios: "servicios" })
-    // res.send("hola 2")
-    //cambio
 })
 
 app.use((req,res,next) => {
-    res.status(404).sendFile(__dirname + "/public/404.html")
+    res.status(404).render("404", {error: "pagina erronea"} )
 })
 
 app.listen(PORT, () => {
